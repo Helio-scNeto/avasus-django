95b3a9415a7c94e21ed2d47cd8a54edcd5c53342
Hélio Neto <hdsn211@gmail.com>
2022-12-26 19:59:04 -0300

    Models
    Models dos professores

--- professores/models.py
+++ professores/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Professor(models.Model):
+    nome = models.CharField(max_length=50)
+    nomeSocial = models.CharField(max_length=50)
+    estado = models.CharField(max_length=50)
+    cidade = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=11)
+    aniversario = models.DateField(max_length=6)
+    def __str__(self) -> str:
+        return self.nome
--- professores/views.py
+++ professores/views.py
@@ -5,4 +5,18 @@ from django.http import HttpResponse
 
 
 def professores(request):
-    return render(request, 'professores.html')
+    if request.method == "GET":
+        return render(request, 'professores.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        nomeSocial = request.POST.get('nomeSocial')
+        estado = request.POST.get('estado')
+        cidade = request.POST.get('cidade')
+        cpf = request.POST.get('cpf')
+        senha = request.POST.get('senha')
+        senhaConfirm = request.POST.get('senhaConfirm')
+        aniversario = request.POST.get('aniversario')
+
+        print(nome)
+
+        return HttpResponse('teste')
